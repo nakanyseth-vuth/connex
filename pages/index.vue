99a9958a771af9da1e762abfa70044844db52ad4
rithchanvirakboth <rithboth0@gmail.com>
2021-12-27 21:29:56 +0700

    add signup page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial />
+  <signup />
 </template>
 
 <script>
+import Signup from '../components/Signup.vue'
 export default {
+  components: { Signup },
   name: 'IndexPage',
 }
 </script>
