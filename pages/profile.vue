d00b3a39e9b224b56f37547509617f78b31a966f
songselasak <song.selasak@gmail.com>
2022-01-03 21:03:44 +0700

    Add Profile

---
+++ pages/profile.vue
@@ -0,0 +1,11 @@
+<template>
+  <Profile />
+</template>
+
+<script>
+import Profile from '../components/Profile.vue'
+export default {
+  components: { Profile },
+  name: 'IndexPage',
+}
+</script>
