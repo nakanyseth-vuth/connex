159fd8e9b6b64a67bbe8d3330c1aa7656a582c5d
rithchanvirakboth <74288085+rithchanvirakboth@users.noreply.github.com>
2022-02-02 15:50:05 +0700

    Update Signup page

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,109 +1,158 @@
 <template>
-<div class="h-screen md:flex">
-	<div
-		class="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
-		<div>
-			<h1 class="text-white font-bold text-4xl font-sans">Connex</h1>
-			<p class="text-white mt-1">The most popular peer to peer lending at SEA</p>
-			<button type="submit" class="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button>
-		</div>
-		<div class="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-		<div class="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-		<div class="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-		<div class="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-	</div>
-	<div class="flex md:w-1/2 justify-center py-10 items-center bg-white">
-		 <form @submit.prevent="submitForm">
-			<h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Friend!</h1>
-			<p class="text-sm font-normal text-gray-600 mb-7">Welcome to Connex</p>
-			<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-					fill="currentColor">
-					<path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-						clip-rule="evenodd" />
-				</svg>
-				<input class="pl-2 outline-none border-none" 
-					type="text" 
-					name="name" 
-					v-model="name"
-					required 
-					placeholder="Full name" />
-      		</div>
-				<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
-						viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-							d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
-					</svg>
-					<input 	class="pl-2 outline-none border-none"
-							type="username" 
-							name="username" 
-							v-model="username"
-							required 
-							placeholder="Username" />
+  <div class="h-screen md:flex">
+    <div
+      class="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden"
+    >
+      <div>
+        <h1 class="text-white font-bold text-4xl font-sans">Connex</h1>
+        <p class="text-white mt-1">
+          The most popular peer to peer lending at SEA
+        </p>
+        <nuxt-link to="/login">
+          <button
+            class="block bg-white text-indigo-800 mt-4 p-2 rounded-2xl font-bold mb-2"
+          >
+            Have an account?
+          </button>
+        </nuxt-link>
       </div>
-					<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
-							viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-								d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-						</svg>
-						<input 	class="pl-2 outline-none border-none" 
-								type="email" 
-								name="email" 
-								v-model="email" 
-								required
-								placeholder="Email Address" />
-      </div>
-						<div class="flex items-center border-2 py-2 px-3 rounded-2xl">
-							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-								fill="currentColor">
-								<path fill-rule="evenodd"
-									d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-									clip-rule="evenodd" />
-							</svg>
-							<input 	class="pl-2 outline-none border-none" 
-									type="password" 
-									name="password" 
-									v-model="password"
-									required
-									placeholder="Password" />
-      </div>
-							<button type="submit" class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Register</button>
-							<span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
-		</form>
-	</div>
-</div>
+      <div
+        class="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"
+      ></div>
+      <div
+        class="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"
+      ></div>
+      <div
+        class="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"
+      ></div>
+      <div
+        class="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"
+      ></div>
+    </div>
+    <div class="flex md:w-1/2 justify-center py-10 items-center bg-white">
+      <form class="bg-white" @submit.prevent="signUp">
+        <h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Friend!</h1>
+        <p class="text-sm font-normal text-gray-600 mb-7">Welcome to Connex</p>
+        <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <input
+            required
+            v-model="name"
+            class="pl-2 outline-none border-none"
+            type="text"
+            name=""
+            id=""
+            placeholder="Full name"
+          />
+        </div>
+       
+        <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+            />
+          </svg>
+          <input
+            required
+            v-model="email"
+            class="pl-2 outline-none border-none"
+            type="text"
+            name=""
+            id=""
+            placeholder="Email Address"
+          />
+        </div>
+        <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <input
+            required
+            v-model="password"
+            class="pl-2 outline-none border-none"
+            type="password"
+            name=""
+            id=""
+            placeholder="Password"
+          />
+        </div>
+        <button
+          type="submit"
+          class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+        >
+          {{ !loading ? 'Sign Up' : 'Loading....' }}
+        </button>
+        <!-- <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer"
+          >Forgot Password ?</span
+        > -->
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-   data() {
-      return {
-         name: '',
-         username: '',
-         email: '',
-         password: '',
+  name: 'Signup',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      loading: false,
+    }
+  },
+  created() {
+    const isAuth = this.$store.getters['users/isAuth']
+    if (isAuth) {
+      this.$router.push('/profile')
+    }
+  },
+  methods: {
+    async signUp() {
+      this.loading = true
+      const { name, email, password } = this
+      const formData = {
+        name,
+        email,
+        password,
       }
-   },
-   methods: {
-	   async submitForm () {
-		    const data = {
-            'name': this.name,
-            'username': this.username,
-            'email': this.email,
-            'password': this.password
-         }
-         console.log(data);
-         try {
-             const res = await this.$axios.post('/api/register', data)
-             
-			 this.$router.push ('/Profile')
-         }
-         catch(e) {
-             console.log(e.message)
-         }
-	   }  
-	   }
+      try {
+        await this.$axios.post('http://localhost:5000/api/register', formData)
+
+        this.$router.push('/Login')
+      } catch (error) {
+        console.log("Error!!")
+      }
+    },
+  },
 }
 </script>
