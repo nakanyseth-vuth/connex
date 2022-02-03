49690ff20ab39b6a132b52c680a0c9a653511b9e
Nakanyseth VUTH <nisethvuth16@gmail.com>
2022-02-04 00:47:23 +0700

    Added homepage decor, Fix Sak codes

--- components/Homepage/Homepage.vue
+++ components/Homepage/Homepage.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
     <PostForm />
-    <Post />
+    <div v-for="post in posts" :key="post._id">
+      <Post :post="post" />
+    </div>
+    <div
+      v-if="posts.length == 0"
+      class="flex w-max flex-col justify-center items-center mt-10 p-5 font-light text-lg rounded-xl mx-auto text-center"
+    >
+      Nothing to show. Post Something!
+      <div class="mt-3 w-[20vw]">
+        <img class="w-full" src="/add_friends.png" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 import Header from '~/components/Utils/Header'
 import Post from '~/components/Homepage/Post'
 import PostForm from '~/components/Homepage/PostForm'
 export default {
+  async asyncData({ store }) {
+    await store.dispatch('post/getAllPosts')
+  },
   components: { Header, Post, PostForm },
+  computed: {
+    ...mapGetters({
+      posts: 'post/getAllPosts',
+    }),
+  },
 }
 </script>
--- components/Homepage/Post.vue
+++ components/Homepage/Post.vue
@@ -1,111 +1,48 @@
 <template>
-  <div>
-    <article class="mt-10">
-      <!-- post card -->
-
-      <div
-        v-for="post in posts"
-        :key="post._id"
-        class="flex bg-white border shadow-lg rounded-lg mx-4 my-5 md:mx-auto w-3/5"
-      >
-        <!--horizantil margin is just for display-->
-        <div class="flex items-start px-4 py-6">
-          <img
-            class="w-12 h-12 rounded-full object-cover mr-4 shadow"
-            :src="profilePrefix + post.user.profileImage"
-            alt="avatar"
-          />
-          <div class="">
-            <div class="flex items-center justify-between">
-              <h2 class="text-lg font-semibold text-gray-900 -mt-1">
-                {{ post.user.name }}
-              </h2>
-            </div>
-            <!-- <p class="text-gray-700">Posted: {{ (post.date_created.split('T')[0]).split('-').reverse().join('-') }}</p> -->
-            <p class="text-gray-700">
-              Posted: {{ $moment(post.date_created).fromNow() }}
-            </p>
-            <p class="mt-3 text-gray-700 text-sm">
-              {{ post.post_text }}
-            </p>
-            <div class="mt-4 flex items-center">
-              <!-- <div class="flex mr-2 text-gray-700 text-sm mr-3">
-                  <svg
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    />
-                  </svg>
-                  <span>12</span>
-                </div>
-                <div class="flex mr-2 text-gray-700 text-sm mr-8">
-                  <svg
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-                    />
-                  </svg>
-                  <span>8</span>
-                </div>
-                <div class="flex mr-2 text-gray-700 text-sm mr-4">
-                  <svg
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                    />
-                  </svg>
-                  <span>share</span>
-                </div> -->
-            </div>
-          </div>
+  <!-- post card -->
+  <div
+    class="flex bg-white border shadow-lg rounded-lg mx-4 my-5 md:mx-auto w-3/5"
+  >
+    <!--horizantil margin is just for display-->
+    <div class="flex items-start px-4 py-6">
+      <img
+        class="w-12 h-12 rounded-full object-cover mr-4 shadow"
+        :src="profilePrefix + post.user.profileImage"
+        alt="avatar"
+      />
+      <div class="">
+        <div class="flex items-center justify-between">
+          <h2 class="text-lg font-semibold text-gray-900 -mt-1">
+            {{ post.user.name }}
+          </h2>
         </div>
+        <!-- <p class="text-gray-700">Posted: {{ (post.date_created.split('T')[0]).split('-').reverse().join('-') }}</p> -->
+        <p class="text-sm font-light text-gray-700">
+          Posted: {{ $moment(post.date_created).fromNow() }}
+        </p>
+        <p class="mt-3 text-gray-700 text-base">
+          {{ post.post_text }}
+        </p>
+        <div class="mt-4 flex items-center"></div>
       </div>
-    </article>
-    <div
-      v-if="posts.length == 0"
-      class="w-1/5 py-3 shadow-md ui-sans-serif text-lg rounded-3xl mx-auto text-center bg-gray-50"
-    >
-      Nothing to show
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 export default {
   async asyncData({ store }) {
-    await store.dispatch('users/getUser')
     await store.dispatch('post/getAllPosts')
   },
   name: 'Post',
+  props: {
+    post: {
+      type: Object,
+    },
+  },
   computed: {
-    ...mapGetters({
-      user: 'users/getUser',
-      posts: 'post/getAllPosts',
-    }),
     profilePrefix() {
-      return this.user.profileImage.includes('uploads')
+      return this.post.user.profileImage.includes('uploads')
         ? 'http://localhost:5000/'
         : ''
     },
